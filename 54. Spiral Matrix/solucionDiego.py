e3342b54ed5b9b0ee2b4b165110bf4db68d1f769
dalejandroabel <79865621+dalejandroabel@users.noreply.github.com>
2023-07-05 21:45:00 -0500

    Create solucionDiego.py

---
+++ 54. Spiral Matrix/solucionDiego.py
@@ -0,0 +1,22 @@
+import numpy as np
+class Solution(object):
+    def spiralOrder(self, matrix):
+        matrix = np.array(matrix)    
+        spiral = []
+        while len(matrix.flatten()) != 0:
+            spiral += list(matrix[0,:][::1])
+            matrix = matrix[1:,:]
+            if len(matrix.flatten()) == 0:
+                break
+            spiral += list(matrix[:,-1][::1])
+            matrix = matrix[:,:-1]
+            if len(matrix.flatten()) == 0:
+                break
+            spiral += list(matrix[-1,:][::-1])
+            matrix = matrix[:-1,:]
+            if len(matrix.flatten()) == 0:
+                break
+            spiral += list(matrix[:,0][::-1])
+            matrix = matrix[:,1:]
+        return spiral
+            
